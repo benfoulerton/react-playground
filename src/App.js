8ec3785da4a43795c83a67694ff6bd06dad13457
Ben Foulerton <benfoulerton@gmail.com>
2025-12-05 22:13:02 +0000

    Learned state - built counter and toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isOn, setisOn] = useState(false);
+  function handleToggle(){
+    setisOn(!isOn);
+  }
   return (
     <div className="App">
       <h1>Ben Foulerton</h1>
-      <p>Portfolio coming soon...</p>
+      <p>The light is {<p>The light is {isOn ? "ON" : "OFF"}</p>}</p>
+      <button onClick={handleToggle}>Toggle Light</button>
     </div>
   );
 }
