4db7ef623a6d8d7ae60917922c0fd8b5f578a630
Ben Foulerton <benfoulerton@gmail.com>
2025-12-07 11:16:16 +0000

    Built working to do list with add functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import './App.css';
 import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [isOn, setisOn] = useState(false);
-  function handleToggle(){
-    setisOn(!isOn);
+  const [tasks, setTasks] = useState(['Buy milk', 'Do homework', 'Learn react']);
+  const [newTask, setNewTask] = useState('');
+  function handleAddTask() {
+  if (newTask.trim() !== '') {
+    setTasks([...tasks, newTask]);
+    setNewTask('');
   }
+}
   return (
     <div className="App">
-      <h1>Ben Foulerton</h1>
-      <p>The light is {<p>The light is {isOn ? "ON" : "OFF"}</p>}</p>
-      <button onClick={handleToggle}>Toggle Light</button>
+      <h1>My To-Do List</h1>
+      <input
+        type = "text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter a new task"
+
+      />
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+      ))}
+      </ul>
+      <button onClick={handleAddTask}>Add Task</button>
     </div>
   );
 }
